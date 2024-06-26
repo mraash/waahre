a2f203b52ad5b6f6dfe5a1bf9cb9ae83fa6b7aae
Eriks Marsans <eriks.marsans@gmail.com>
2024-06-26 21:51:54 +0300

    init project

---
+++ tests/IndexTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+    public function testFirst(): void
+    {
+        $this->assertTrue(true);
+    }
+}
